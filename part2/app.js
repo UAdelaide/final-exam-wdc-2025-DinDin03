26c6b73e7f5b7fe38d9d863fce79ffa2fcffb92b
DinDin03 <dineth.himasha@outlook.com>
2025-06-20 08:35:08 +0000

    [automatic commit] 2025-06-20 08:35:08 UTC 175.35.81.9

--- part2/app.js
+++ part2/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 require('dotenv').config();
 const app = express();
-const session = require('express-session'); // Added session import
+const session = require('express-session');
 
 app.use(session({
   secret: process.env.SESSION_SECRET || 'key',
